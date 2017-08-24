ef1b2f5512da2ce3e1f0a488dfebc2dfbe775ab8
jighead <gerdsuhr@gmail.com>
2017-08-24 14:26:57 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin'); 
+
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        // publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            { // used for multiple pages. They can be imported in app.js. In this example project it's second.html and third.html
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                        }
+                    }
+                ],
+                exclude: path.resolve(__dirname, 'src/index.html')
+            },
+            {
+                test: /\.(jpg|png|svg|gif|ico)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist']) // removes dist folder after build when using the webpack dev server. Run build:prod to see the dist folder.
+    ]
+};
